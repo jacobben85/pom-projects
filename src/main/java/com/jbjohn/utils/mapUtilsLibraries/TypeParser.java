cfa211505b153811ccd4c8f35fb98e29b32bead8
jacobben85 <jacobben85@gmail.com>
2015-09-24 21:38:35 -0400

    Exception handling
    Exception handling

--- src/main/java/com/jbjohn/utils/mapUtilsLibraries/TypeParser.java
+++ src/main/java/com/jbjohn/utils/mapUtilsLibraries/TypeParser.java
@@ -123,30 +123,34 @@ public class TypeParser {
 
     private static Object getValue(Object value) {
         Object response = value;
-        switch (type) {
-            case STRING:
-                if (!(value instanceof String)) {
-                    response = String.valueOf(value);
-                }
-                break;
-            case INTEGER:
-                if (!(value instanceof Integer) && value.toString().matches("^-?\\d+$")) {
-                    response = Integer.parseInt(value.toString());
-                }
-                break;
-            case BOOLEAN:
-                if (!(value instanceof Boolean) && (value.toString().equalsIgnoreCase("true") || value.toString().equalsIgnoreCase("false"))) {
-                    response = Boolean.parseBoolean(value.toString());
-                }
-                break;
-            case FLOAT:
-                if (!(value instanceof Float) && value.toString().matches("^-?\\d+(?:[.]\\d+)$")) {
-                    response = Float.parseFloat(value.toString());
-                }
-                break;
-            default:
-                response = value;
-                break;
+        try {
+            switch (type) {
+                case STRING:
+                    if (!(value instanceof String)) {
+                        response = String.valueOf(value);
+                    }
+                    break;
+                case INTEGER:
+                    if (!(value instanceof Integer) && value.toString().matches("^-?\\d+$")) {
+                        response = Integer.parseInt(value.toString());
+                    }
+                    break;
+                case BOOLEAN:
+                    if (!(value instanceof Boolean) && (value.toString().equalsIgnoreCase("true") || value.toString().equalsIgnoreCase("false"))) {
+                        response = Boolean.parseBoolean(value.toString());
+                    }
+                    break;
+                case FLOAT:
+                    if (!(value instanceof Float) && value.toString().matches("^-?\\d+(?:[.]\\d+)$")) {
+                        response = Float.parseFloat(value.toString());
+                    }
+                    break;
+                default:
+                    response = value;
+                    break;
+            }
+        } catch (Exception e) {
+            // TODO log exception
         }
         return response;
     }
