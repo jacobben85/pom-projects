a8937dd2cd1f84c7b014854b4938bfd59aa3d841
jacobben85 <jacobben85@gmail.com>
2015-09-28 13:33:27 -0400

    updates to test script
    updates to test scripts

--- src/test/java/com/jbjohn/deportes/StandaloneTest.java
+++ src/test/java/com/jbjohn/deportes/StandaloneTest.java
@@ -12,13 +12,18 @@ public class StandaloneTest {
     public void testProcess() {
         Standalone standalone = new Standalone();
         try {
+
             String eventId = "832461";
+            String startTime = "20150922T000000-0500";
+            String endTime = "20150924T230000-0500";
+
+            String fixtureKeys = "event-stats,event-stats-progressive";
             String url = "http://feed5.xmlteam.com/api/feeds?" +
-                    "start=20150922T000000-0500" +
-                    "&end=20150924T230000-0500" +
+                    "start=" + startTime +
+                    "&end=" + endTime +
                     "&publisher-keys=optasports.com" +
                     "&sport-keys=15054000" +
-                    "&fixture-keys=event-stats,event-stats-progressive" +
+                    "&fixture-keys=" + fixtureKeys +
                     "&format=xml" +
                     "&event-keys=EFBO";
             standalone.fetchLinksAndProcess(url + eventId, eventId);
